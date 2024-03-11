b3a42bfaa2bdde85deaafff7e6ba3ef6eb1c496b
Dengju Deng <wj871287@gmail.com>
2024-03-11 11:10:45 +0800

    fix: 更新 replicate 结构

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'replicate.delivery',
-            port: '',
-            pathname: '/pbxt/**',
-          },
-        ],
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
       },
-}
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
